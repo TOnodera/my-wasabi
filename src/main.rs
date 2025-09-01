e970497d20ec8b743717b4cba701be35d2ba21f4
onodera <onodera@h-company.co.jp>
2025-09-01 20:33:45 +0900

    p-60まで実装

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@
 #![no_main]
 
 #[no_mangle]
-fn efi_main() {
-    // println!("Hello, world!");
-    loop {
-         
+fn efi_main(_image_handle: EfiHandle, _efi_system_table: &EfiSystemTable) {
+    let efi_graphics_output_protocol = locate_graphic_protocol(_efi_system_table).unwrap();
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts(vram_addr as *mut u32, vram_byte_size / size_of::<u32>(), len)
+    };
+    
+    for e in vram {
+        *e = 0xffffff;
     }
+    
+    loop {}
 }
 
 use::core::panic::PanicInfo;
+use core::slice;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
