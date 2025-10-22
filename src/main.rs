ef0b4983e1c6182d40ab4c28334809a6ecf0592d
onodera <onodera@h-company.co.jp>
2025-10-22 20:03:14 +0900

    コード整理途中まで

--- src/main.rs
+++ src/main.rs
@@ -1,215 +1,22 @@
 #![no_std]
 #![no_main]
-#![feature(offset_of)]
 
 use core::arch::asm;
-use core::fmt;
-use core::fmt::Write;
-use core::mem::offset_of;
-use core::mem::size_of;
 use core::panic::PanicInfo;
-use core::ptr::null_mut;
 use core::writeln;
 
-use wasabi::graphics::draw_font_fg;
-use wasabi::graphics::draw_line;
-use wasabi::graphics::draw_str_fg;
-use wasabi::graphics::fill_rect;
-use wasabi::graphics::Bitmap;
-use wasabi::result::Result;
-
-type EfiVoid = u8;
-type EfiHandle = u64;
-
-#[repr(C)]
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-struct EfiGuid {
-    pub data0: u32,
-    pub data1: u16,
-    pub data2: u16,
-    pub data3: [u8; 8],
-}
-
-const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID: EfiGuid = EfiGuid {
-    data0: 0x9042a9de,
-    data1: 0x23dc,
-    data2: 0x4a38,
-    data3: [0x96, 0xfb, 0x7a, 0xde, 0xd0, 0x80, 0x51, 0x6a],
+use wasabi::graphics::{draw_test_pattern, fill_rect};
+use wasabi::uefi::{
+    exit_from_efi_services, init_vram, EfiHandle, EfiMemoryType, EfiSystemTable, MemoryMapHolder,
+    VramTextWriter,
 };
-
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-#[must_use]
-#[repr(u64)]
-enum EfiStatus {
-    Success = 0,
-}
-
-#[repr(i64)] // Cと安定して連携するために必要
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-#[allow(non_camel_case_types)]
-pub enum EfiMemoryType {
-    RESERVED = 0,
-    LOADER_CODE,
-    LOADER_DATA,
-    BOOT_SERVICES_CODE,
-    BOOT_SERVICES_DATA,
-    RUNTIME_SERVICES_CODE,
-    RUNTIME_SERVICES_DATA,
-    CONVENTIONAL_MEMORY,
-    UNUSABLE_MEMORY,
-    ACPI_RECLAIM_MEMORY,
-    ACPI_MEMORY_NVS,
-    MEMORY_MAPPED_IO,
-    MEMORY_MAPPED_IO_PORT_SPACE,
-    PAL_CODE,
-    PERSISTENT_MEMORY,
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-struct EfiMemoryDescriptor {
-    memory_type: EfiMemoryType,
-    phisical_start: u64,
-    virtual_start: u64,
-    number_of_pages: u64,
-    attribute: u64,
-}
-
-const MEMORY_MAP_BUFFER_SIZE: usize = 0x8000;
-
-struct MemoryMapHolder {
-    memory_map_buffer: [u8; MEMORY_MAP_BUFFER_SIZE],
-    memory_map_size: usize,
-    map_key: usize,
-    descriptor_size: usize,
-    descriptor_version: u32,
-}
-
-struct MemoryMapIterator<'a> {
-    map: &'a MemoryMapHolder,
-    ofs: usize,
-}
-impl<'a> Iterator for MemoryMapIterator<'a> {
-    type Item = &'a EfiMemoryDescriptor;
-    fn next(&mut self) -> Option<&'a EfiMemoryDescriptor> {
-        if self.ofs >= self.map.memory_map_size {
-            None
-        } else {
-            let e: &EfiMemoryDescriptor = unsafe {
-                &*(self.map.memory_map_buffer.as_ptr().add(self.ofs) as *const EfiMemoryDescriptor)
-            };
-            self.ofs += self.map.descriptor_size;
-            Some(e)
-        }
-    }
-}
-
-impl MemoryMapHolder {
-    pub const fn new() -> MemoryMapHolder {
-        MemoryMapHolder {
-            memory_map_buffer: [0; MEMORY_MAP_BUFFER_SIZE],
-            memory_map_size: MEMORY_MAP_BUFFER_SIZE,
-            map_key: 0,
-            descriptor_size: 0,
-            descriptor_version: 0,
-        }
-    }
-    pub fn iter(&self) -> MemoryMapIterator {
-        MemoryMapIterator { map: self, ofs: 0 }
-    }
-}
-
-#[repr(C)]
-struct EfiBootServicesTable {
-    _reserved0: [u64; 7],
-    get_memory_map: extern "win64" fn(
-        memory_map_size: *mut usize,
-        memory_map: *mut u8,
-        map_key: *mut usize,
-        descriptor_size: *mut usize,
-        descriptor_version: *mut u32,
-    ) -> EfiStatus,
-    _reserved1: [u64; 21],
-    exit_boot_services: extern "win64" fn(image_handle: EfiHandle, map_key: usize) -> EfiStatus,
-    _reserved4: [u64; 10],
-    locate_protocol: extern "win64" fn(
-        protocol: *const EfiGuid,
-        registration: *const EfiVoid,
-        interface: *mut *mut EfiVoid,
-    ) -> EfiStatus,
-}
-
-impl EfiBootServicesTable {
-    fn get_memory_map(&self, map: &mut MemoryMapHolder) -> EfiStatus {
-        (self.get_memory_map)(
-            &mut map.memory_map_size,
-            map.memory_map_buffer.as_mut_ptr(),
-            &mut map.map_key,
-            &mut map.descriptor_size,
-            &mut map.descriptor_version,
-        )
-    }
-}
-const _: () = assert!(offset_of!(EfiBootServicesTable, get_memory_map) == 56);
-const _: () = assert!(offset_of!(EfiBootServicesTable, exit_boot_services) == 232);
-const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
-
-#[repr(C)]
-struct EfiSystemTable {
-    _reserved0: [u64; 12],
-    pub boot_services: &'static EfiBootServicesTable,
-}
-const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96);
-
-#[repr(C)]
-#[derive(Debug)]
-struct EfiGraphicsOutputProtocolPixelInfo {
-    version: u32,
-    pub horizontal_resolution: u32,
-    pub vertical_resolution: u32,
-    _padding0: [u32; 5],
-    pub pixels_per_scan_line: u32,
-}
-const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
-
-#[repr(C)]
-#[derive(Debug)]
-struct EfiGraphicsOutputProtocolMode<'a> {
-    pub max_mode: u32,
-    pub mode: u32,
-    pub info: &'a EfiGraphicsOutputProtocolPixelInfo,
-    pub size_of_info: u64,
-    pub frame_buffer_base: usize,
-    pub frame_buffer_size: usize,
-}
-
-#[repr(C)]
-#[derive(Debug)]
-struct EfiGraphicsOutputProtocol<'a> {
-    reserved: [u64; 3],
-    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
-}
-fn locate_graphic_protocol<'a>(
-    efi_system_table: &EfiSystemTable,
-) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
-    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
-    let status = (efi_system_table.boot_services.locate_protocol)(
-        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
-        null_mut::<EfiVoid>(),
-        &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol as *mut *mut EfiVoid,
-    );
-    if status != EfiStatus::Success {
-        return Err("Failed to locate graphics protocol");
-    }
-    Ok(unsafe { &*graphic_output_protocol })
-}
 
 #[no_mangle]
 fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let mut vram = init_vram(efi_system_table).expect("Failed to initialize VRAM");
 
-    let vw = vram.width;
-    let vh = vram.height;
+    let vw = vram.width();
+    let vh = vram.height();
 
     fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("fill_rect failed.");
     draw_test_pattern(&mut vram);
@@ -220,15 +27,15 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 
     let mut memory_map = MemoryMapHolder::new();
     let status = efi_system_table
-        .boot_services
+        .boot_services()
         .get_memory_map(&mut memory_map);
     writeln!(w, "{status:?}").unwrap();
     let mut total_memory_pages = 0;
     for e in memory_map.iter() {
-        if e.memory_type != EfiMemoryType::CONVENTIONAL_MEMORY {
+        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
             continue;
         }
-        total_memory_pages += e.number_of_pages;
+        total_memory_pages += e.number_of_pages();
         writeln!(w, "{e:?}").unwrap();
     }
     let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
@@ -254,110 +61,5 @@ pub fn hlt() {
 fn panic(_info: &PanicInfo) -> ! {
     loop {
         hlt()
-    }
-}
-
-#[derive(Clone, Copy)]
-struct VramBufferInfo {
-    buf: *mut u8,
-    width: i64,
-    height: i64,
-    pixels_per_line: i64,
-}
-
-impl Bitmap for VramBufferInfo {
-    fn bytes_per_pixel(&self) -> i64 {
-        4
-    }
-    fn pixels_per_line(&self) -> i64 {
-        self.pixels_per_line
-    }
-    fn width(&self) -> i64 {
-        self.width
-    }
-    fn height(&self) -> i64 {
-        self.height
-    }
-    fn buf_mut(&mut self) -> *mut u8 {
-        self.buf
-    }
-}
-
-fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
-    let gp = locate_graphic_protocol(efi_system_table)?;
-    Ok(VramBufferInfo {
-        buf: gp.mode.frame_buffer_base as *mut u8,
-        width: gp.mode.info.horizontal_resolution as i64,
-        height: gp.mode.info.vertical_resolution as i64,
-        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
-    })
-}
-
-struct VramTextWriter<'a> {
-    vram: &'a mut VramBufferInfo,
-    cursor_x: i64,
-    cursor_y: i64,
-}
-
-impl<'a> VramTextWriter<'a> {
-    fn new(vram: &'a mut VramBufferInfo) -> Self {
-        Self {
-            vram,
-            cursor_x: 0,
-            cursor_y: 0,
-        }
-    }
-}
-
-impl fmt::Write for VramTextWriter<'_> {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            if c == '\n' {
-                self.cursor_y += 16;
-                self.cursor_x = 0;
-                continue;
-            } else {
-                draw_font_fg(self.vram, self.cursor_x, self.cursor_y, 0xffffff, c);
-                self.cursor_x += 8;
-            }
-        }
-        Ok(())
-    }
-}
-
-fn draw_test_pattern<T: Bitmap>(buf: &mut T) {
-    let w = 128;
-    let left = buf.width() - w - 1;
-    let colors = [0x000000, 0xff0000, 0x00ff00, 0x0000ff];
-    let h = 64;
-
-    for (i, c) in colors.iter().enumerate() {
-        let y = i as i64 * h;
-        fill_rect(buf, *c, left, y, h, h).expect("fill_rect failed.");
-        fill_rect(buf, !*c, left + h, y, h, h).expect("fill_rect failed.");
-    }
-
-    let points = [(0, 0), (0, w), (w, 0), (w, w)];
-    for (x0, y0) in points.iter() {
-        for (x1, y1) in points.iter() {
-            let _ = draw_line(buf, 0xffffff, left + *x0, *y0, left + *x1, *y1);
-        }
-    }
-    draw_str_fg(buf, left, h * colors.len() as i64, 0x00ff00, "0123456789");
-    draw_str_fg(buf, left, h * colors.len() as i64 + 16, 0x00ff00, "ABCDEF");
-}
-
-fn exit_from_efi_services(
-    image_handle: EfiHandle,
-    efi_system_table: &EfiSystemTable,
-    memory_map: &MemoryMapHolder,
-) {
-    loop {
-        let status =
-            (efi_system_table.boot_services.exit_boot_services)(image_handle, memory_map.map_key);
-        assert_eq!(status, EfiStatus::Success);
-        if status == EfiStatus::Success {
-            break;
-        }
     }
 }
