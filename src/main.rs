52dffdae357ddb8c8f1000bdd5c19844ee132351
onodera <onodera@h-company.co.jp>
2025-11-16 18:50:52 +0900

    cargo test, cargo runの両方できるようになった。

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::writeln;
 use wasabi::graphics::{draw_test_pattern, fill_rect, Bitmap};
+use wasabi::init::init_basic_runtime;
 use wasabi::qemu::exit_qemu;
 use wasabi::qemu::QemuExitCode;
 use wasabi::uefi::{
-    exit_from_efi_services, init_vram, EfiHandle, EfiMemoryType,
-    EfiSystemTable, MemoryMapHolder, VramTextWriter,
+    init_vram, EfiHandle, EfiMemoryType, EfiSystemTable, VramTextWriter,
 };
 use wasabi::x86::hlt;
 
@@ -23,15 +23,8 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("fill_rect failed.");
     draw_test_pattern(&mut vram);
     let mut w = VramTextWriter::new(&mut vram);
-    for i in 0..4 {
-        writeln!(w, "i = {i:?}").unwrap();
-    }
 
-    let mut memory_map = MemoryMapHolder::new();
-    let status = efi_system_table
-        .boot_services()
-        .get_memory_map(&mut memory_map);
-    writeln!(w, "{status:?}").unwrap();
+    let memory_map = init_basic_runtime(image_handle, efi_system_table);
     let mut total_memory_pages = 0;
     for e in memory_map.iter() {
         if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
@@ -47,11 +40,6 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     )
     .unwrap();
 
-    exit_from_efi_boot_services(
-        image_handle,
-        efi_system_table,
-        &mut memory_map,
-    );
     writeln!(w, "Hello, Non-EFI World!").unwrap();
 
     loop {
