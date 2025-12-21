bddbefaee9658d949d9f69893431c40854fc067d
onodera <onodera@h-company.co.jp>
2025-12-21 11:07:23 +0900

    GDT,IDTの設定まで

--- src/x86.rs
+++ src/x86.rs
@@ -7,6 +7,7 @@ use alloc::boxed::Box;
 use core::arch::asm;
 use core::arch::global_asm;
 use core::fmt;
+use core::fmt::Display;
 use core::marker::PhantomData;
 use core::mem::offset_of;
 use core::mem::size_of;
@@ -674,5 +675,164 @@ impl TaskStateSegment64 {
         };
         info!("TSS64 craeted @ {:#X}", this.phys_addr());
         this
+    }
+}
+impl Drop for TaskStateSegment64 {
+    fn drop(&mut self) {
+        panic!("TSS64 being dropped!");
+    }
+}
+
+pub fn init_exceptions() -> (GdtWrapper, Idt) {
+    let gdt = GdtWrapper::default();
+    gdt.load();
+    unsafe {
+        // コードセグメント(今どの特権レベルで実行されているかを示す)
+        // mv cs, ... ←この操作はできない
+        write_cs(KERNEL_CS);
+        // スタックセグメント
+        write_ss(KERNEL_DS);
+        // 文字列命令用セグメント(歴史的経緯で互換性の為に存在)
+        write_es(KERNEL_DS);
+        // データセグメント(歴史的経緯で互換性の為に存在)
+        write_ds(KERNEL_DS);
+        // 汎用セグメント(TSLスレッドローカル, スレッド固有データへの高速アクセス)
+        write_fs(KERNEL_DS);
+        // 汎用セグメント(per-CPU/カーネル用)
+        write_gs(KERNEL_DS);
+    }
+    let idt = Idt::new(KERNEL_CS);
+    (gdt, idt)
+}
+
+pub const BIT_TYPE_DATA: u64 = 0b10u64 << 43;
+pub const BIT_TYPE_CODE: u64 = 0b11u64 << 43;
+
+pub const BIT_PRESENT: u64 = 1u64 << 47;
+pub const BIT_CS_LONG_MODE: u64 = 1u64 << 53;
+pub const BIT_CS_READABLE: u64 = 1u64 << 53;
+pub const BIT_DS_WRITABLE: u64 = 1u64 << 41;
+pub const BIT_DPL0: u64 = 0u64 << 45;
+pub const BIT_DPL3: u64 = 3u64 << 45;
+
+#[repr(u64)]
+enum GdtAttr {
+    KernelCode = 
+        BIT_TYPE_CODE | BIT_PRESENT | BIT_CS_LONG_MODE | BIT_CS_READABLE ,
+    KernelData = 
+        BIT_TYPE_DATA | BIT_PRESENT | BIT_DS_WRITABLE,
+}
+
+#[allow(dead_code)]
+#[repr(C, packed)]
+struct GdtParameters {
+    limit: u16,
+    base: *const Gdt,
+}
+
+pub const KERNEL_CS : u16 = 1 << 3;
+pub const KERNEL_DS : u16 = 2 << 3;
+pub const TSS64_SEGMENT : u16 = 3 << 3;
+
+#[allow(dead_code)]
+#[repr(C, packed)]
+pub struct Gdt {
+    null_segment: GdtSegmentDescriptor,
+    kernel_code_segment: GdtSegmentDescriptor,
+    kernel_data_segment: GdtSegmentDescriptor,
+    task_state_segment: TaskStateSegment64Descriptor,
+}
+const _ :() = assert!(size_of::<Gdt>() == 40);
+
+#[allow(dead_code)]
+pub struct GdtWrapper {
+    inner: Pin<Box<Gdt>>,
+    tss64: TaskStateSegment64,
+}
+
+impl GdtWrapper {
+    pub fn load(&self)
+    {
+        let params = GdtrPrameters {
+            limit: (size_of::<Gdt>() - 1) as u16,
+            base: self.inner.as_ref().get_ref() as *const Gdt,
+        };
+        info!("Loading GDT @ {params:#018X}");
+        unsafe {
+            // 命令名：LGDT (Load Global Descriptor Table Register)
+            // 役割：GDTR（GDT レジスタ）に GDT の情報をロード
+            // いつ使う？：ブート時 / カーネル初期化時
+            // 権限：特権命令（ring 0）
+            // 
+            // 汎用レジスタ RCX に GDT の情報を指すポインタをセットし、
+            // LGDT 命令で GDTR にロードする。
+            // csxにはGdtrPrameters構造体のアドレスが入っている
+            asm!("lgdt [rcx]", in("rcx") &params);
+
+        };
+    }
+}
+impl Default for GdtWrapper {
+    fn default() -> Self {
+        let tss64 = TaskStateSegment64::new();
+        let gdt = Gdt {
+            null_segment: GdtSegmentDescriptor::null(),
+            kernel_code_segment: GdtSegmentDescriptor::new(GdtAttr::KernelCode),
+            kernel_data_segment: GdtSegmentDescriptor::new(GdtAttr::KernelData),
+            task_state_segment: TaskStateSegment64Descriptor::new(tss64.phys_addr()),
+        };
+        Self {
+            inner: Box::pin(gdt),
+            tss64,
+        }
+    }
+}
+
+pub struct GdtSegmentDescriptor {
+    value: u64,
+}
+impl GdtSegmentDescriptor {
+    fn null() -> Self {
+        Self { value: 0 }
+    }
+    fn new(attr: GdtAttr) -> Self {
+        Self { value: attr as u64 }
+    }
+}   
+impl Display for GdtSegmentDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#018X}", self.value)
+    }
+}
+
+#[repr(C, packed)]
+#[allow(dead_code)]
+struct TaskStateSegment64Descriptor {
+    limit_low: u16,
+    base_low: u16,
+    base_mid_low: u8,
+    attr: u16,
+    base_mid_hight: u32,
+    base_high: u32,
+    reserved: u32,
+}
+impl TaskStateSegment64Descriptor {
+    fn new(base_addr: u64) -> Self {
+        Self {
+            limit_low: size_of::<TaskStateSegment64Inner>() as u16,
+            base_low: (base_addr & 0xffff) as u16,
+            base_mid_low: ((base_addr >> 16) & 0xff) as u8,
+            attr: 0b1000_0000_1000_0001, 
+            base_mid_hight: ((base_addr >> 24) & 0xff) as u32,
+            base_high: ((base_addr >> 32) & 0xffffffff) as u32,
+            reserved: 0,
+        }
+    }
+}
+const _: () = assert!(size_of::<TaskStateSegment64Descriptor>() == 16);
+
+pub fn trigger_debug_interrupt() {
+    unsafe {
+        asm!("int3");
     }
 }
