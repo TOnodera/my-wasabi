72b2df51435d7a2c262a9386ef4e7a67adc5e820
onodera <onodera@h-company.co.jp>
2026-01-29 19:45:33 +0900

    pciの一覧を表示できるところまで

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -15,6 +15,7 @@ qemu-system-x86_64 \
     -bios thrid_party/ovmf/RELEASEX64_OVMF.fd \
     -machine q35 \
     -drive format=raw,file=fat:rw:mnt \
+    -monitor telnet:0.0.0.0:2345,server,nowait,logfile=log/monitor.txt \
     -chardev stdio,id=char_com1,mux=on,logfile=log/com1.txt \
     -serial chardev:char_com1 \
     -device isa-debug-exit,iobase=0xf4,iosize=0x01
