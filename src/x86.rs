9fab8d94f6442865bcb0c70e029f9e3962e1579d
onodera <onodera@h-company.co.jp>
2026-02-13 20:45:20 +0900

    p-343まで実装したが、エラーが発生している

--- src/x86.rs
+++ src/x86.rs
@@ -15,6 +15,7 @@ use core::mem::size_of_val;
 use core::mem::MaybeUninit;
 use core::panic;
 use core::pin::Pin;
+use core::mem::ManuallyDrop;
 
 pub fn hlt() {
     unsafe { asm!("hlt") }
@@ -942,3 +943,20 @@ pub fn flush_tlb() {
         write_cr3(read_cr3());
     }
 }
+
+pub unsafe fn take_current_page_table() -> ManuallyDrop<Box<PML4>> {
+    ManuallyDrop::new(Box::from_raw(read_cr3()))
+}
+
+pub unsafe fn put_current_page_table(mut table: ManuallyDrop<Box<PML4>>) {
+    write_cr3(Box::into_raw(ManuallyDrop::take(&mut table)));
+}
+
+pub unsafe fn with_current_page_table<F>(callback: F)
+where 
+    F: FnOnce(&mut PML4),
+    {
+        let mut table = take_current_page_table();
+        callback(&mut table);
+        put_current_page_table(table);
+    }
